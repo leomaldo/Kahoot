4201c7a06dd2005e1c088a4c19126f992982235e
MohaLourd <128591509+MohaLourd@users.noreply.github.com>
2024-03-09 21:18:12 +0100

    Update Servidor.c
    SERVIDOR MODIFICADO,  va todo.
    Si no entendeis algo me decis.

--- Servidor.c
+++ Servidor.c
@@ -5,118 +5,223 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <mysql/mysql.h>
 
-int main(int argc, char *argv[])
-{
+typedef struct {
+	int id_partida;
+	int id_usuario;
+	int preguntas_correctas;
+} Partida;
+
+typedef struct {
+	int id_usuario;
+	char nombre_usuario[100];
+} Jugador;
+
+char* JugadorMaxPtsTotales(MYSQL *conn) {
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	int err;
+	char jugador[100];
 	
-	int sock_conn, sock_listen, ret;
-	struct sockaddr_in serv_adr;
-	char peticion[512];
-	char respuesta[512];
-	// INICIALITZACIONS
-	// Obrim el socket
-	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		printf("Error creant socket");
-	// Fem el bind al port
+	err = mysql_query(conn, "SELECT nombre_usuario, numero_puntos_totales FROM jugadores ORDER BY numero_puntos_totales DESC");
 	
-	
-	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
-	serv_adr.sin_family = AF_INET;
-	
-	// asocia el socket a cualquiera de las IP de la m?quina. 
-	//htonl formatea el numero que recibe al formato necesario
-	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// establecemos el puerto de escucha
-	serv_adr.sin_port = htons(9000);
-	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
-		printf ("Error al bind");
-	
-	if (listen(sock_listen, 3) < 0)
-		printf("Error en el Listen");
-	
-	int i;
-	// Bucle infinito
-	for (;;){
-		printf ("Escuchando\n");
-		
-		sock_conn = accept(sock_listen, NULL, NULL);
-		printf ("He recibido conexion\n");
-		//sock_conn es el socket que usaremos para este cliente
-		
-		int terminar =0;
-		// Entramos en un bucle para atender todas las peticiones de este cliente
-		//hasta que se desconecte
-		while (terminar ==0)
-		{
-			// Ahora recibimos la petici?n
-			ret=read(sock_conn,peticion, sizeof(peticion));
-			printf ("Recibido\n");
-			
-			// Tenemos que a?adirle la marca de fin de string 
-			// para que no escriba lo que hay despues en el buffer
-			peticion[ret]='\0';
-			
-			
-			printf ("Peticion: %s\n",peticion);
-			
-			// vamos a ver que quieren
-			char *p = strtok( peticion, "/");
-			int codigo =  atoi (p);
-			// Ya tenemos el c?digo de la petici?n
-			char nombre[20];
-
-			if (codigo !=0)
-			{	
-				//Establecemos conexi髇 con la base de datos
-                MYSQL *conn = mysql_init(NULL);
-				if (conn == NULL) {
-					fprintf(stderr, "Error al inicializar la conexion: %s\n", mysql_error(conn));
-					return 1;
-				}
-				if (mysql_real_connect(conn, "localhost", "root", "mysql", "Kahoot", 0, NULL, 0) == NULL) {
-					fprintf(stderr, "Error al conectarse a la base de datos: %s\n", mysql_error(conn));
-					mysql_close(conn);
-					return 1;
-				}
-	
-				            printf("Conexion exitosa a la base de datos Kahoot\n");
-							// Mostramos el codigo
-							printf ("Codigo: %d\n", codigo);
-			}
-
-			if (codigo ==0) //petici?n de desconexi?n
-				terminar=1;
-			else if (codigo ==1) //piden la m醲ima puntuaci髇
-			    {
-
-				//Aqui pondriamos nuestra respuesta de la maxima puntuacion
-				sprintf (respuesta,"%d",strlen (nombre));
-				 }
-			else if (codigo == 2) //piden el jugador con mas puntos
-			     {
-
-       			 }
-			else if (codigo == 3) //piden la partida con menos preguntas correctas
-			     {
-
-       			 }
-			else if (codigo == 4) //quieren logearse
-			     {
-
-       			 }
-			else  //quieren registrarse
-			     {
-
-       			 }
-						
-			if (codigo !=0)
-			{				
-				printf ("Respuesta: %s\n", respuesta);
-				// Enviamos respuesta
-				write (sock_conn,respuesta, strlen(respuesta));
-			}
-		}
-		// Se acabo el servicio para este cliente
-		close(sock_conn); 		
+	if (err != 0) {
+		printf("Error al consultar datos de la base %u %s\n", mysql_errno(conn), mysql_error(conn));
+		return NULL;
 	}
+	resultado = mysql_store_result(conn);
+	
+	row = mysql_fetch_row(resultado);
+	
+	if (row == NULL) {
+		printf("No se han obtenido datos en la consulta\n");
+		return NULL;
+	}
+	
+	strcpy(jugador, row[0]);
+	
+	mysql_free_result(resultado);
+    printf("%s",jugador);
+	char* jugadorNombre = strdup(jugador); // Duplicar la cadena para evitar problemas de memoria
+	return jugadorNombre;;
+}
+int PtsDeLaPartidaConMasPts(MYSQL *conn) {
+	int puntosMasPuntos = -1; // Valor inicial para indicar que no se encontraron partidas
+	
+	char query[200];
+	sprintf(query, "SELECT puntos FROM partida ORDER BY puntos DESC LIMIT 1");
+	
+	if (mysql_query(conn, query)) {
+		fprintf(stderr, "Error al ejecutar la consulta: %s\n", mysql_error(conn));
+		return -1;
+	}
+	MYSQL_RES *result = mysql_store_result(conn);
+	
+	if (result == NULL) {
+		fprintf(stderr, "Error al obtener el resultado: %s\n", mysql_error(conn));
+		return -1;
+	}
+	
+	MYSQL_ROW row = mysql_fetch_row(result);
+	
+	if (row != NULL) {
+		puntosMasPuntos = atoi(row[0]);
+	} else {
+		printf("No se encontraron partidas.\n");
+	}
+	mysql_free_result(result);
+	printf("%d",puntosMasPuntos);
+	
+	return puntosMasPuntos;
+}
+int encontrarPartidaMenosCorrectas(MYSQL *conn) {
+	char query[200];
+	sprintf(query, "SELECT ID_partida FROM partida ORDER BY numero_preguntas_correctas ASC LIMIT 1");
+	
+	if (mysql_query(conn, query)) {
+		fprintf(stderr, "Error al ejecutar la consulta: %s\n", mysql_error(conn));
+		return -1;
+	}
+	
+	MYSQL_RES *result = mysql_store_result(conn);
+	
+	if (result == NULL) {
+		fprintf(stderr, "Error al obtener el resultado: %s\n", mysql_error(conn));
+		return -1;
+	}
+	MYSQL_ROW row = mysql_fetch_row(result);
+	
+	if (row != NULL) {
+		int partidaId = atoi(row[0]);
+		mysql_free_result(result);
+		return partidaId;
+	} else {
+		printf("No se encontraron partidas.\n");
+		mysql_free_result(result);
+		return -1;
+	}
+}
+
+void registro(MYSQL *conn, const char *usuario, const char *contrasena) {
+	char query[100];
+	sprintf(query, "INSERT INTO usuarios (usuario, contrasena) VALUES ('%s', '%s')", usuario, contrasena);
+	
+	if (mysql_query(conn, query)) {
+		fprintf(stderr, "Error al insertar datos: %s\n", mysql_error(conn));
+	} else {
+		printf("Registro exitoso para usuario: %s\n", usuario);
+	}
+}
+
+int verificarCredenciales(MYSQL *conn, const char *usuario, const char *contrasena) {
+	char query[100];
+	sprintf(query, "SELECT * FROM usuarios WHERE usuario='%s' AND contrasena='%s'", usuario, contrasena);
+	
+	if (mysql_query(conn, query)) {
+		fprintf(stderr, "Error al verificar credenciales: %s\n", mysql_error(conn));
+		return 0;
+	}
+	MYSQL_RES *result = mysql_store_result(conn);
+	int num_rows = mysql_num_rows(result);
+	
+	mysql_free_result(result);
+	
+	return num_rows > 0;
+}
+
+
+void handleClientRequest(int clientSocket, MYSQL *conn) {
+	char request[512];
+	char response[512];
+	
+	while (1) {
+		int ret = read(clientSocket, request, sizeof(request));
+		if (ret <= 0) {
+			printf("Cliente desconectado\n");
+			break;
+		}
+		
+		request[ret] = '\0';
+	int code = atoi(strtok(request, "/"));
+	
+	
+	if (code == 2) {
+		char* res = JugadorMaxPtsTotales(conn);
+		sprintf(response, "%s", res);
+	} else if (code == 1) {
+		int res2= PtsDeLaPartidaConMasPts(conn);
+		sprintf(response, "%d", res2);
+	} else if (code == 3) {
+		int res3 = encontrarPartidaMenosCorrectas(conn);
+		sprintf(response, "%d", res3);
+	}else if (code == 5) {
+		char *usuario = strtok(NULL, "/");
+		char *contrasena = strtok(NULL, "/");
+		
+		registro(conn, usuario, contrasena);
+		
+		sprintf(response, "Registro exitoso para usuario: %s", usuario);
+	}else if (code == 4) {
+		char *usuario = strtok(NULL, "/");
+		char *contrasena = strtok(NULL, "/");
+		
+		// Verificar las credenciales en la base de datos
+		if (verificarCredenciales(conn, usuario, contrasena)) {
+			sprintf(response, "Inicio de sesi贸n exitoso para usuario: %s", usuario);
+		}else {
+			sprintf(response, "Credenciales incorrectas");
+		}
+	} else {
+		sprintf(response, "C贸digo no v谩lido");
+	}
+
+	
+	write(clientSocket, response, strlen(response));
+	}
+}
+int main() {
+	MYSQL *conn = mysql_init(NULL);
+	
+	if (conn == NULL) {
+		fprintf(stderr, "Error al inicializar la conexi贸n: %s\n", mysql_error(conn));
+		return 1;
+	}
+	
+	if (mysql_real_connect(conn, "localhost", "root", "mysql", "Kahoot", 0, NULL, 0) == NULL) {
+		fprintf(stderr, "Error al conectarse a la base de datos: %s\n", mysql_error(conn));
+		mysql_close(conn);
+		return 1;
+	}
+	printf("Conexi贸n exitosa a la base de datos Kahoot\n");
+	
+	int serverSocket, clientSocket;
+	struct sockaddr_in serverAddr, clientAddr;
+	socklen_t addrLen = sizeof(struct sockaddr_in);
+	
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(9052);
+	bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	listen(serverSocket, 3);
+	
+	while (1) {
+		printf("Esperando conexiones...\n");
+		
+		int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &addrLen);
+		
+		printf("Cliente conectado\n");
+		
+		handleClientRequest(clientSocket, conn);
+		
+		close(clientSocket);
+	}
+	close(serverSocket);
+	
+	mysql_close(conn);
+	
+	return 0;
 }
